66980d69eef974b7d0f98f4304a834eac798fa2d
lblok <sendtolucy@gmail.com>
2025-02-24 09:16:25 -0500

    updated methodology to use the beginning of school years

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# LAschoolconversions
- Analysis and data visuals for LA school conversions project
+# Analysis and data visuals for LA school conversions project
+
+`starting-salaries-rent-burden.R` pulls median gross rents for LA city (Census designated place) from ACS 5-Year Estimates for 2018-2023 (B25064_001) and compares them to LA Unified School District starting teacher salaries.
+
+### Salary data
+
+We used salary tables from the [LAUSD Human Resources Division](https://www.lausd.org/page/4045) website. We used the L tables for teachers with Alternative Certification Salaries. Due to nationwide teacher shortages, most districts aren't able to recruit fully certified teachers any more, so they recruit "preparation" teachers with alternative credentials who take additional classes at night to get their full credentials while working full time. We took the lowest salary from L tables for school years starting 2018-2023 and manually compiled those in a [Google Sheet](https://docs.google.com/spreadsheets/d/11G4uKbqWofdGmR5c6yxZmzIrXMTDFqQPIu55Biwerj8/edit?usp=sharing) and downloaded as a CSV: `salary data - l-tables.csv`. We compared the beginning of the school year to rents for the same year, e.g. starting salaries in July-December 2023 of the 2023-2024 school year were compared to median gross rents in 2023.
--- starting-salaries-rent-burden.R
+++ starting-salaries-rent-burden.R
@@ -3,10 +3,10 @@ library(tidycensus)
 library(janitor)
 
 v23 <- load_variables(2023, "acs5", cache = TRUE)
-# B25031_001 Estimate!!Median gross rent --!!Total:  Median Gross Rent by Bedrooms
+# Variable used: B25064_001 | Estimate!!Median gross rent | Median Gross Rent (Dollars)
 
-# This function pulls median gross rents from ACS 5-year estimates 2019-2023 and compiles them into a single data frame. Note that these 5-year estimates overalap with each other which generally isn't advised, but I manually checked against 1-year estimates and the results were similar - plus 1-year estimates are missing for the year 2020.  
-get_rent_data_5 <- function(start_year = 2019, end_year = 2023) {
+# This function pulls median gross rents from ACS 5-year estimates 2018-2023 and compiles them into a single data frame. Note that these 5-year estimates overalap with each other which generally isn't advised, but I manually checked against 1-year estimates and the results were similar - plus 1-year estimates are missing for the year 2020.  
+get_rent_data_5 <- function(start_year = 2018, end_year = 2023) {
   purrr::map_dfr(start_year:end_year, ~ {
     get_acs(
       geography = "place", 
@@ -25,7 +25,7 @@ get_rent_data_5 <- function(start_year = 2019, end_year = 2023) {
 # Run the function
 rent_data_5 <- get_rent_data_5()
 
-# salary data - l-tables.csv is a CSV downloaded from the manually created 'salary data.gsheet' in this folder. That data is hand-compiled from https://www.lausd.org/page/4045 using the lowest salary for teachers with alternate certifications.
+# salary data - l-tables.csv is a CSV downloaded from the manually created 'salary data.gsheet' in this folder. That data is hand-compiled from https://www.lausd.org/page/4045 using the lowest salary for teachers with alternate certifications. See README for more details.
 starting_salary <- read_csv('salary data - l-tables.csv') %>%
   pivot_longer(cols = -classification, names_to = "year", values_to = "starting_salary") %>%
   mutate(year = as.numeric(year))
@@ -46,8 +46,8 @@ starting_salary_rent_plot <- ggplot(starting_salary_rent_data, aes(year, ratio))
     panel.grid.minor.x = element_blank(),
     panel.grid.minor.y = element_blank()
   ) +
-  scale_x_continuous(breaks = starting_salary_zillow$year) +
-  scale_y_continuous(limits = c(35, 45), breaks = seq(0,100, by = 1))
+  scale_x_continuous(breaks = starting_salary_rent_data$year) +
+  scale_y_continuous(limits = c(35, 43), breaks = seq(0,100, by = 1))
 
 
 # Save the plot as an SVG file. I then modified it in Illustrator for the memo.
